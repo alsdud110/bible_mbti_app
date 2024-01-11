d75188b355073da8d68c91afbfb25ec5fbf7ac7d
alsdud110 <alsdud110@naver.cpm>
2024-01-11 23:09:40 +0900

    bottomNav 변경

--- lib/screen/main/tab/tab_item.dart
+++ lib/screen/main/tab/tab_item.dart
@@ -1,11 +1,11 @@
 import 'package:bible_mbti_app/common/common.dart';
 import 'package:bible_mbti_app/screen/main/tab/home/f_home.dart';
-import 'package:bible_mbti_app/screen/main/tab/profile/f_profile.dart';
+import 'package:bible_mbti_app/screen/main/tab/result/f_result.dart';
 import 'package:flutter/material.dart';
 
 enum TabItem {
-  home(Icons.home, '홈', HomeFragment()),
-  profile(Icons.person, '마이페이지', ProfileFragment());
+  home(Icons.home, '테스트', HomeFragment()),
+  profile(Icons.person, '내 결과', ResultFragment());
   // favorite(Icons.star, '즐겨찾기', FavoriteFragment(isShowBackButton: false));
 
   final IconData activeIcon;
