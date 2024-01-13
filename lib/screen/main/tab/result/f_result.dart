0640e6e4c7faba26077e342dba6a5c9f75a68a32
alsdud110 <alsdud110@naver.cpm>
2024-01-13 10:53:49 +0900

    결과에 따른 이미지 및 설명 VO, map 추가 후 ResultFragment 꾸미기

--- lib/screen/main/tab/result/f_result.dart
+++ lib/screen/main/tab/result/f_result.dart
@@ -1,4 +1,6 @@
+import 'package:bible_mbti_app/common/constants.dart';
 import 'package:bible_mbti_app/screen/main/tab/home/vo/result_type.dart';
+import 'package:bible_mbti_app/screen/main/tab/result/vo/resultVO.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -16,8 +18,9 @@ class _ResultFragmentState extends State<ResultFragment>
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: "나의 성경 인물 속 MBTI는?".text.make(),
-      ),
+          title: resultData.result.isEmpty
+              ? "나의 성경 인물 속 MBTI는?".text.make()
+              : resultData.result.value.text.size(30).bold.make()),
       body: Container(
         padding: const EdgeInsets.symmetric(horizontal: 28),
         child: Obx(
@@ -29,7 +32,49 @@ class _ResultFragmentState extends State<ResultFragment>
                     .size(22)
                     .make()
                     .centered()
-                : resultData.result.value.text.bold.size(28).make(),
+                : Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(170),
+                        child: Image.asset(
+                          "$baseMbtiPath/${resultMap[resultData.result.value]?.mbtiImgPath}",
+                          width: double.infinity,
+                          height: 300,
+                        ),
+                      ).pOnly(top: 20),
+                      "${resultMap[resultData.result.value]?.mbti}"
+                          .text
+                          .bold
+                          .size(28)
+                          .make()
+                          .pOnly(top: 16),
+                      "${resultMap[resultData.result.value]?.person}"
+                          .text
+                          .bold
+                          .size(20)
+                          .make(),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          "• ${resultMap[resultData.result.value]?.char1}"
+                              .text
+                              .size(16)
+                              .make()
+                              .pOnly(bottom: 8),
+                          "• ${resultMap[resultData.result.value]?.char2}"
+                              .text
+                              .size(16)
+                              .make()
+                              .pOnly(bottom: 8),
+                          "• ${resultMap[resultData.result.value]?.char3}"
+                              .text
+                              .size(16)
+                              .make()
+                              .pOnly(bottom: 8),
+                        ],
+                      ).pOnly(top: 26),
+                    ],
+                  ).pSymmetric(h: 16),
           ),
         ),
       ),
---
+++ lib/screen/main/tab/result/vo/resultVO.dart
@@ -0,0 +1,105 @@
+class ResultVO {
+  final String mbti;
+  final String person;
+  final String mbtiImgPath;
+  final String char1;
+  final String char2;
+  final String char3;
+
+  ResultVO(
+      {required this.mbti,
+      required this.person,
+      String? mbtiImgPath,
+      required this.char1,
+      required this.char2,
+      required this.char3})
+      : mbtiImgPath = mbtiImgPath ?? "$mbti.png";
+}
+
+Map<String, ResultVO> resultMap = {
+  "ENTJ": ResultVO(
+      mbti: "ENTJ",
+      person: "사도 바울",
+      char1: "이끌고 싶어 하는 욕구가 강하고 가장 열정적",
+      char2: "조화를 위해 하나님의 사랑에 초점을 맞춰야 하는 타입",
+      char3: "리더쉽과 의사소통의 대가로 잘 알려진 말씀의 해설자"),
+  "ENTP": ResultVO(
+      mbti: "ENTP",
+      person: "에훗",
+      char1: "세상과 말씀을 잘 융합할 수 있는 이야기꾼, 은유법의 달인",
+      char2: "다른 사람들을 알려주기 위한 교훈의 은사를 갖고 있음",
+      char3: "극한의 위기에서 상황 판단 단번에 하고 제일 먼저 탈출"),
+  "ESFJ": ResultVO(
+      mbti: "ESFJ",
+      person: "요한",
+      char1: "다른 이를 섬기고, 일하는 것을 통해 자신의 헌신적인 면을 보여줌",
+      char2: "하나님과 다른 이들과의 친밀한 관계를 통해 힘을 얻고, 그들을 위해 봉사함",
+      char3: "사회생활 잘한다는 소리를 자주 들음"),
+  "ESFP": ResultVO(
+      mbti: "ESFP",
+      person: "야고보",
+      char1: "행동 지향적이며, 자기 삻을 통해 믿음을 보여줌",
+      char2: "하나님에 대해 친구 같은 따뜻한 감정의 소유자",
+      char3: "예배 때 찬양 부르기 좋아함"),
+  "ESTJ": ResultVO(
+      mbti: "ESTJ",
+      person: "여호수아",
+      char1: "교회의 기둥이며 봉사에 적극적",
+      char2: "말씀의 원리를 설명하는 데 있어 노련하고 강한 신념을 가짐",
+      char3: "교회의 전통에 충실하고, 논리와 현실성과 역사의 가치를 잘 알고 있음"),
+  "ESTP": ResultVO(
+      mbti: "ESTP",
+      person: "스가랴",
+      char1: "자발적이고 하나님께 복종할수록 점점 안정되는 믿음을 가진 사람",
+      char2: "다수가 교회에 등질 때 기꺼이 자신을 희생함",
+      char3: "갈등이 심해질수록 머리는 차가워짐"),
+  "INTJ": ResultVO(
+      mbti: "INTJ",
+      person: "모세",
+      char1: "위대한 믿음을 갖고 있고, 헌신적이기 때문에 순교적인 인물이 될 수 있음",
+      char2: "어려운 언어나 이국적인 음식, 또는 위험한 환경 등에 크게 영향을 받지 않음",
+      char3: "성경을 읽고 해석하는데 뛰어난 재능을 가짐"),
+  "INFJ": ResultVO(
+      mbti: "INFJ",
+      person: "예레미야",
+      char1: "기도, 예배 그리고 하나님과의 교제를 중요하게 여김",
+      char2: "다른 사람의 아픔과 시련을 함께 해줄 수 있는 사람",
+      char3: "기도와 영적인 여정을 즐기는 사람"),
+  "INFP": ResultVO(
+      mbti: "INFP",
+      person: "세례 요한",
+      char1: "엄청난 사랑의 소유자이며, 다른 사람을 위하는 마음이 큼",
+      char2: "나만의 신념, 가치관을 갖고 있음",
+      char3: "우유부단하며 일을 잘 미룸"),
+  "INTP": ResultVO(
+      mbti: "INTP",
+      person: "솔로몬",
+      char1: "논리적 설명, 과학적 사실과 지식을 중요시함",
+      char2: "토론과 설교를 중요시함",
+      char3: "자신이 믿는 것에 대해 이해와 목적을 갈망하는 타입"),
+  "ISFJ": ResultVO(
+      mbti: "ISFJ",
+      person: "마리아",
+      char1: "뒤에서 안 보이게 섬기고 의생할 수 있는 사람",
+      char2: "다른 이들을 설득하고 동기를 부여할 수 있는 신념을 가짐",
+      char3: "계획 세우는 것을 좋아하고 계획대로 일이 진행되어야 마음이 편함"),
+  "ISFP": ResultVO(
+      mbti: "ISFP",
+      person: "아담",
+      char1: "영적인 감정들과 생각들을 조용히, 깊이 있게 대하는 사람",
+      char2: "따스한 사람이고, 책임감과 때론 죄책감까지도 짊어짐",
+      char3: "기도와 예배를 매일 드림에 있어서 자연스럽고, 실천을 통해 자신을 나타냄"),
+};
+
+// ENTJ - 바울
+// ENTP - 에훗
+// ESFJ - 요한
+// ESFP - 야고보
+// ESTJ - 여호수아
+// ESTP - 스가랴
+// INFJ - 예레미야
+// INFP - 세례요한
+// INTJ - 모세
+// INTP - 솔로몬
+// ISFJ - 마리아 
+// ISFP - 아담
