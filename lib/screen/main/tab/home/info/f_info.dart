5fd4836b86e238b52e67e39b06de88f0806b0bb9
alsdud110 <alsdud110@naver.cpm>
2024-01-22 22:57:59 +0900

    일단은.... 지원하지 않는(못하는) 기능ㅠㅠ

--- lib/screen/main/tab/home/info/f_info.dart
+++ lib/screen/main/tab/home/info/f_info.dart
@@ -1,5 +1,6 @@
 import 'package:bible_mbti_app/common/common.dart';
 import 'package:bible_mbti_app/common/widget/w_menu_container.dart';
+import 'package:bible_mbti_app/screen/dialog/d_message.dart';
 import 'package:bible_mbti_app/screen/main/tab/home/info/w_mbti_log.dart';
 import 'package:bible_mbti_app/screen/main/tab/home/info/w_setting_theme.dart';
 import 'package:flutter/cupertino.dart';
@@ -53,18 +54,33 @@ class InfoFragment extends StatelessWidget {
             height: 0.5,
             thickness: 0.5,
           ),
-          Container(
-            width: double.infinity,
-            height: 45,
-            padding: const EdgeInsets.only(left: 26),
-            decoration:
-                BoxDecoration(color: context.appColors.infoTextBackgound),
-            child: Row(
-              children: [
-                const Icon(CupertinoIcons.globe),
-                width10,
-                "지역 및 언어".text.size(16).color(context.appColors.infoText).make()
-              ],
+          Tap(
+            onTap: () {
+              final result = MessageDialog(
+                "아직 지원하지 않는 기능입니다.",
+                textAlign: TextAlign.center,
+                buttonText: "닫기",
+                fontSize: 16,
+              ).show();
+              debugPrint(result.toString());
+            },
+            child: Container(
+              width: double.infinity,
+              height: 45,
+              padding: const EdgeInsets.only(left: 26),
+              decoration:
+                  BoxDecoration(color: context.appColors.infoTextBackgound),
+              child: Row(
+                children: [
+                  const Icon(CupertinoIcons.globe),
+                  width10,
+                  "지역 및 언어"
+                      .text
+                      .size(16)
+                      .color(context.appColors.infoText)
+                      .make()
+                ],
+              ),
             ),
           ),
           MenuContainer(
